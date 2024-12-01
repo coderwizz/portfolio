e50830c618a92d55bfa659c94de199b00e9cb467
coderwizz <129561256+coderwizz@users.noreply.github.com>
2024-12-01 13:53:16 -0800

    Update experience.tsx

--- components/experience.tsx
+++ components/experience.tsx
@@ -42,7 +42,7 @@ const projects = [
     id: 6,
     name: "COMPOSE Leaderboard Prototype",
     description: "Built using Next.js, SQL, and ??? a prototype for the leaderboard now currently being used by the entire 70-person organization.",
-    imageUrl: "/assets/putnam.png",
+    imageUrl: "/assets/ldr.png",
     link: "https://drive.google.com/file/d/1UWhDU9tVRIpAIQarwwmb8U9G8UpG1gnI/view?usp=drive_link",
   },
   {
