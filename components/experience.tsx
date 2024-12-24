296a24be954a14d6ce7c8b0c947a6cc9dcb08b19
coderwizz <129561256+coderwizz@users.noreply.github.com>
2024-12-23 23:28:48 -0500

    Update experience.tsx

--- components/experience.tsx
+++ components/experience.tsx
@@ -62,7 +62,7 @@ const projects = [
   {
     id: 9,
     name: "Math Contest Awards",
-    description: "Relevant Awards: Top 200, William Lowell Putnam Mathematical Competition 2023; Qualifier, USA Mathematical Olympiad (USA(J)MO) 2019-2022; 5th Place, Stanford Math Tournament (Number Theory) 2021. At above: a humorous response by google’s AI when searching \“putnam 2023 top 200\”.",
+    description: "Developed and tested a script to fine-tune LLaMA-2 7B with AWS and QLoRA. Resolved a critical bug, saving a key project for a team of eight despite senior developer absence. Collaborated with two sub-teams on data loaders, GPU management, and introspection to ensure smooth integration. Built an ML model classifying 20 bird species as part of SemEval onboarding.",
     imageUrl: "/assets/putnam.png",
     link: "https://drive.google.com/file/d/1UWhDU9tVRIpAIQarwwmb8U9G8UpG1gnI/view?usp=drive_link",
   },
